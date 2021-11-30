d2aeae3d30b7484be8eb3038a5c739e06b8350ad
LeHuuPhu <phulealali@gmail.com>
2021-11-30 17:47:49 +0700

    done api employees

--- back-end/src/routes/employees.route.js
+++ back-end/src/routes/employees.route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const HttpException = require('../../src/util/HttpException.utils');
 const EmployeesController = require('../app/controllers/employees.controller');
 
 router.post('/login', EmployeesController.Login);
@@ -8,4 +9,13 @@ router.post('/account/create', EmployeesController.CreateAccount);
 router.get('/account/:id', EmployeesController.AccountById);
 router.get('/account/clock/:id', EmployeesController.ClockAccountById);
 router.put('/account/update', EmployeesController.UpdateAccount);
+router.post('/list', EmployeesController.ListEmployees);
+router.get('/:id', EmployeesController.EmployeesByID);
+router.get('/clock/:id', EmployeesController.EmployeesClockById);
+router.post('/create', EmployeesController.EmployeesCreate);
+router.put('/update', EmployeesController.EmployeesUpdate);
+router.all('*', (req, res, next) => {
+    const err = new HttpException(404, 'Endpoint Not Found');
+    next(err);
+  });
 module.exports = router;
